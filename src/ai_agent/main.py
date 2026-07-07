d1f2322baf46251ca481fe0fc8d50661171dbd03
rw42 <roguewizard42@gmail.com>
2026-07-07 12:46:24 -0400

    swap out gemini for openrouter API and rework the code in main

--- src/ai_agent/main.py
+++ src/ai_agent/main.py
@@ -2,47 +2,48 @@
 
 import os
 from dotenv import load_dotenv
-from google import genai
-from google.genai.types import GenerateContentResponse, Content, Part
+from openai import OpenAI
+from openai.types.chat import ChatCompletion, ChatCompletionMessageParam
 from argparse import ArgumentParser, Namespace
 
 load_dotenv()
-api_key: str | None = os.environ.get("GEMINI_API_KEY")
+api_key: str | None = os.environ.get("OPENROUTER_API_KEY")
 if api_key is None:
     raise RuntimeError("The API key was not found")
 
 parser: ArgumentParser = ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
-parser.add_argument("--verbose", action="store_true", help="Rnable verbose output")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args: Namespace = parser.parse_args()
 
 if not isinstance(args.user_prompt, str):
     raise RuntimeError("The user prompt must be a string")
 
-messages: list[Content] = [Content(role="user", parts=[Part(text=args.user_prompt)])]
+messages: list[ChatCompletionMessageParam] = [
+    {"role": "user", "content": args.user_prompt}
+]
 
-client: genai.Client = genai.Client(api_key=api_key)
-response: GenerateContentResponse = client.models.generate_content(
-    model='gemini-2.5-flash', contents=messages
-    )
+client: OpenAI = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+response: ChatCompletion = client.chat.completions.create(
+    model="openrouter/free",
+    messages=messages,
+)
 
-if response.usage_metadata is None:
+if response.usage is None:
     raise RuntimeError("No usage metadata returned")
 
-prompt_tokens: int | None =  response.usage_metadata.prompt_token_count
-response_tokens: int | None = response.usage_metadata.candidates_token_count
+prompt_tokens: int = response.usage.prompt_tokens
+response_tokens: int = response.usage.completion_tokens
 
+content: str | None = response.choices[0].message.content
 
 if args.verbose:
     print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens:{prompt_tokens}")
     print(f"Response tokens: {response_tokens}")
-    print(response.text)
+    print(content)
 else:
-    print(response.text)
-
-#print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-#print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-#print(response.text)
-
-
+    print(content)
