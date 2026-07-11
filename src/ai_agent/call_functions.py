7636670bcabafb50074b81e81b983ce170569809
rw42 <roguewizard42@gmail.com>
2026-07-11 15:37:35 -0400

    add JSON schemas to call_functions | tweak system prompt with function options list

--- src/ai_agent/call_functions.py
+++ src/ai_agent/call_functions.py
@@ -2,7 +2,15 @@
 
 
 from ai_agent.functions.get_files_info import schema_get_files_info
+from ai_agent.functions.get_file_content import schema_get_file_content
+from ai_agent.functions.write_file import schema_write_file
+from ai_agent.functions.run_python_file import schema_run_python_file
+
+
 
 available_functions = [
     schema_get_files_info,
+    schema_get_file_content,
+    schema_write_file,
+    schema_run_python_file,
 ]
