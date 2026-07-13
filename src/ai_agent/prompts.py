e279854c1b18baff3e8c260448bd6ee5d3732527
rw42 <roguewizard42@gmail.com>
2026-07-13 17:22:10 -0400

    final test passed: agent fix a sabotage in the calculator function correctly

--- src/ai_agent/prompts.py
+++ src/ai_agent/prompts.py
@@ -1,7 +1,7 @@
 # system prompts for the bot
 
 system_prompt: str = """
-    You are a helpful AI coding agent.
+    You are a helpful Autonomous AI coding agent.
 
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
@@ -10,8 +10,13 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Execute Python files with optional arguments
 - Write or overwrite files
 
+When a user reports a bug, begin with your get_files_info tool on the working directory to get orientated. Then explore subdirectories instead of guessing. 
+- before reporting that a bug has been fixed be sure to run the test files to confirm that something else has not been broken.
+- if the tests failed, you have not fixed the bug
+- a bug fix is successful only if the tests still pass.
+
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 
-Don't ask the user for more input until you've tried using your tools first. If all tools fail, then ask for more information.
+Don't ask the user for more input until you've tried using your tools first. If all tools fail, then ask for further instructions.
 """
 
